8d397d023f4b3a56788c06c44367adf881a96d1e
Eduardo Aliaga <aaaacccc_aaaacccc@hotmail.com>
2016-10-25 14:12:06 -0500

    falta

---
+++ geometry.cpp
@@ -0,0 +1,50 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+
+class Point
+{
+    public:
+        Point(int , int );
+        int getX() const;
+        int getY() const;
+        void setX(const int new_x);
+        void setY(const int new_y);
+
+        virtual ~Point();
+
+    private:
+        int x, y;
+};
+
+class PointArray
+{
+    public:
+        PointArray(Point , int );
+        PointArray(const Point points[], const int new_size);
+        PointArray(const PointArray &pv);
+        void resize(int );
+        virtual ~PointArray();
+        void push_back(const Point & );
+        void insert(const int , const Point & );
+        void remove(const int );
+        const int getSize() const;
+        void clear();
+
+    private:
+        int size;
+        Point *points;
+
+};
+
+class Polygon
+{
+    public:
+        Polygon(PointArray , static int );
+
+    protected:
+        PointArray p;
+        static int x;
+}
+
+#endif // GEOMETRY_H
