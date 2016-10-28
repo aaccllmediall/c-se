5ad56f7eae6841fac1442a20488ee3e26d96cf05
Eduardo Aliaga <aaaacccc_aaaacccc@hotmail.com>
2016-10-28 17:31:55 -0500

    :v
    .....................................

--- geometry.h
+++ geometry.h
@@ -33,17 +33,42 @@ class PointArray
     private:
         int size;
         Point *points;
-
 };
 
 class Polygon
 {
     public:
         Polygon(PointArray , static int );
+        int getNumPolygons(static int );
+        int getNumSides(int );
+        Point getPoints(int ,int );
+        double area();
 
     protected:
         PointArray p;
         static int x;
 }
+class Rectangle : public Polygon
+{
+    public:
+        Rectangle(Point ,Point );
+        double area();
+        
+    protected:
+        Point x;
+        Point y;
+};
+class Triangle : public Polygon
+{
+    public:
+        Triangle(Point , Point , Point );
+        double area();
+        
+        
+    protected:
+        Point x;
+        Point y;
+        Point z;
+};
 
 #endif // GEOMETRY_H
