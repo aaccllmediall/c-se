0d02921ca0ebf7c456e103f8af0b268377f88410
Eduardo Aliaga <aaaacccc_aaaacccc@hotmail.com>
2016-11-08 11:03:07 -0500

    yes
    ...............................

--- Point.cpp
+++ Point.cpp
@@ -1,27 +1,43 @@
-#include "Point.h"
+#include <iostream>
+
+using namespace std;
 
 template <typename T>
-Point::Point(T x, T y)
+class Point
 {
-    this->x=x;
-    this->y=y;
-}
-T Point::getX(){
-    return x;
-}
-T Point::getX(){
-    return y;
-}
-void Point::setX(T x2){
-    this->x=x2;
-}
-void Point::setY(T y2){
-    this->y=y2;
-}
+    public:
+        Point(const T x, const T y) : x(x), y(y) {}
+        T getX() { return x; }
+        T getY() { return y; }
+        void setX(T x2) { x(x2); }
+        void setY(T y2) { y(y2); }
 
-Point::~Point()
-{
-    //dtor
-}
+    public:
+        T x, y;
+};
 
 template <typename T>
+Point<T> operator+(const Point<T> o, const Point<T> w)
+{
+    Point<T> aux={0, 0};
+    aux.x=w.x+o.x;
+    aux.y=w.y+o.y;
+    return aux;
+};
+
+template <typename T>
+ostream& operator<<(ostream &output, const Point<T> &o)
+{
+    output << "(" << o.x << ", " << o.y << ")" << endl;
+    return output;
+};
+
+main()
+{
+    cout << "Hello world!" << endl;
+    Point<int> a(1, 4);
+    Point<int> b(3, 7);
+    Point<int> c=a+b;
+    cout << c << endl;
+    cout << a.getX() << endl;
+}
